4e88a51390e27c203436609f1d63ef518cae0596
Chike <chikespott@gmail.com>
2017-07-06 16:34:27 +0100

    initial commit

---
+++ app/src/main/java/com/chikeandroid/tutsplusandroidosmstoken/SmsTokenResultVerificationActivity.java
@@ -0,0 +1,16 @@
+package com.chikeandroid.tutsplusandroidosmstoken;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Chike on 7/5/2017.
+ */
+public class SmsTokenResultVerificationActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sms_token_result);
+    }
+}
